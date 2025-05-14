083ba8750344a20710e78334e5b4bd780b331d0a
JMCRmeteorito <jose.castano.rojas@correounivalle.edu.co>
2025-05-13 20:09:18 -0500

    Implementando primeras funciones para la vista consola

--- src/vista/VistaPokemonConsola.java
+++ src/vista/VistaPokemonConsola.java
@@ -1,26 +1,71 @@
 package vista;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import controlador.Controlador;
 import modelo.Entrenador;
 import modelo.Pokemon;
 
 public class VistaPokemonConsola implements VistaPokemon {
+    private Scanner scanner;
+    
 
     @Override
     public void bienvenido() {
-        // TODO Auto-generated method stub
+        System.out.println(""" 
+                    Estás a punto de sumergirte en un
+                    mundo lleno de aventuras de las
+                    que vas a ser protagonista.
+
+                    Te cruzarás con rivales y criaturas
+                    salvajes que querrán luchar contigo,
+                    pero ¡ánimo, tú puedes!""");
+        
         throw new UnsupportedOperationException("Unimplemented method 'bienvenido'");
     }
 
     @Override
     public void entrenadores() {
+        scanner = new Scanner(System.in);
+        System.out.print("Ingrese el nombre del Entrenador 1: ");
+        nombre1 = scanner.nextLine();
+        System.out.print("Ingrese el nombre del Entrenador 2: ");
+        nombre2 = scanner.nextLine();
+        String nombre1 = "Jugador 1";
+        String nombre2 = "Jugador 2";
+        String texto = """
+            Bienvenidos a Pokémon {jugador1} y 
+                {jugador2} les espera un gran 
+                desafío en su aventura.
+
+                Sus pokemones serán asignados 
+                aleatoriamente, y tendrán que 
+                enfrentarse para demostrar 
+                quién es el mejor entrenador.
+            """;
+        texto = texto.replace("{jugador1}", nombre1).replace("{jugador2}", nombre2);
+        System.out.println(texto);
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'entrenadores'");
     }
 
     @Override
     public void pokemones() {
-        // TODO Auto-generated method stub
+        scanner = new Scanner(System.in);
+        System.out.print("Ingrese el nombre del primer Pokémon: ");
+        String pokemon1 = scanner.nextLine();
+        System.out.print("Ingrese el nombre del segundo Pokémon: ");
+        String pokemon2 = scanner.nextLine();
+        System.out.print("Ingrese el nombre del tercer Pokémon: ");
+        String pokemon3 = scanner.nextLine();
+
+        System.out.println("Los Pokémon ingresados son:");
+        System.out.println("1. " + pokemon1);
+        System.out.println("2. " + pokemon2);
+        System.out.println("3. " + pokemon3);
         throw new UnsupportedOperationException("Unimplemented method 'pokemones'");
     }
 
